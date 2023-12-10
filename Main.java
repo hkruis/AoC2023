c90c591a4922c76461b6e6890ebbe112f7505de7
hkruis <153369269+hkruis@users.noreply.github.com>
2023-12-10 17:22:26 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println(Day1.addFromText());
+        System.out.println(Day2.reaedGames());
     }
 }
